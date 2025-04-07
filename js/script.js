4be59ec5140a2ec6207a274b1a0c0df180a6c6fc
Mateusz Matysek <mateusziom1@gmail.com>
2025-04-07 12:31:05 +0200

    Add code for fillter buttons in script.js

--- js/script.js
+++ js/script.js
@@ -40,17 +40,72 @@ const makeWigets = function(){
             html += boxElement;
         });
         document.querySelector('.wrapper-ext').innerHTML += html;
-        
+
+        /* checkbox checked if is-active attribute is true on start*/
+        document.querySelectorAll('.box-ext').forEach(box => {
+            if(box.getAttribute('is-active') === 'true'){
+                box.querySelector('input').checked = true;
+            }else if(box.getAttribute('is-active') === 'false'){
+                box.querySelector('input').checked = false;
+            }
+        });
+
     });
 }
 
 makeWigets();
 
-
+/* Change Widget Status */
+document.querySelector('.wrapper-ext').addEventListener('change', function(e){
+    if(e.target.id === 'filter-slider'){
+        const box = e.target.closest('.box-ext');
+        if(e.target.checked){
+            box.setAttribute('is-active', 'true');
+        }else{
+            box.setAttribute('is-active', 'false');
+        }
+    }
+});
 
 /* Remove Widget */
 document.querySelector('.wrapper-ext').addEventListener('click', function(e){
     if(e.target.classList.contains('btn-rm')){
         e.target.closest('.box-ext').remove();
+    }
+});
+
+// if filter buttton is cliced add active class and remove active class from other buttons
+document.querySelector('.buttons-filter').addEventListener('click', function(e){
+    if(e.target.classList.contains('btn-filter')){
+        document.querySelectorAll('.btn-filter').forEach(btn => {
+            btn.classList.remove('active');
+        });
+        e.target.classList.add('active');
+    }
+});
+
+/* Filter bar 'All' 'Active' 'Inactive' */
+document.querySelector('.buttons-filter').addEventListener('click', function(e){
+    if(e.target.classList.contains('btn-filter')){
+        document.querySelectorAll('.box-ext').forEach(box => {
+            box.style.display = 'none';
+        });
+        if(e.target.textContent === 'All'){
+            document.querySelectorAll('.box-ext').forEach(box => {
+                box.style.display = 'flex';
+            });
+        }else if(e.target.textContent === 'Active'){
+            document.querySelectorAll('.box-ext').forEach(box => {
+                if(box.getAttribute('is-active') === 'true'){
+                    box.style.display = 'flex';
+                }
+            });
+        }else if(e.target.textContent === 'Inactive'){
+            document.querySelectorAll('.box-ext').forEach(box => {
+                if(box.getAttribute('is-active') === 'false'){
+                    box.style.display = 'flex';
+                }
+            });
+        }
     }
 });
